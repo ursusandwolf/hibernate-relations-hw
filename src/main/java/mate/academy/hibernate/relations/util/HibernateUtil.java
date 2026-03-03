4558fd0de79be42252b5b42e1bd4166ef95e5dbf
alex <ursusandwolf@protonmail.com>
2026-03-03 20:17:27 +0200

    fix config

--- src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
+++ src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
@@ -1,9 +1,45 @@
 package mate.academy.hibernate.relations.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
+    private static final SessionFactory instance = initSessionFactory();
+
+    private HibernateUtil() {
+    }
+
     public static SessionFactory getSessionFactory() {
-        return null;
+        return instance;
+    }
+
+    private static SessionFactory initSessionFactory() {
+        Configuration configuration = new Configuration();
+        configuration.configure(); // hibernate.cfg.xml
+
+        Properties properties = new Properties();
+
+        try (InputStream input = Thread.currentThread().getContextClassLoader()
+                .getResourceAsStream("db.properties")) {
+
+            if (input == null) {
+                throw new RuntimeException("db.properties not found in resources");
+            }
+
+            properties.load(input);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load DB properties", e);
+        }
+
+        configuration.setProperty("hibernate.connection.username",
+                properties.getProperty("db.username"));
+        configuration.setProperty("hibernate.connection.password",
+                properties.getProperty("db.password"));
+
+        return configuration.buildSessionFactory();
     }
 }
+
